b394bca76e6892ebab03a97d40996dbc9a42d3b4
levinsousa <techie.levinsousa@gmail.com>
2020-10-20 17:25:39 -0300

    Commit

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,80 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm'
+import Orphanage from '../models/Orphanages'
+import orphanageView from '../views/orphanages_view'
+import * as Yup from 'yup'
+
+export default {
+  async index(req: Request, res: Response){
+    const orphanageRepository = getRepository(Orphanage)
+    const orphanages = await orphanageRepository.find({
+      relations: ['images']
+    })
+
+    return res.json(orphanageView.renderMany(orphanages));
+  },
+  async show(req: Request, res: Response){
+    const { id } = req.params;
+
+    const orphanageRepository = getRepository(Orphanage)
+    const orphanage = await orphanageRepository.findOneOrFail(id,{
+      relations: ['images']
+    })
+
+    return res.json(orphanageView.render(orphanage));
+  },
+
+  async create(req: Request, res: Response){
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    } = req.body;
+  
+    const orphanageRepository = getRepository(Orphanage)
+  
+    const requestImages = req.files as Express.Multer.File[];
+
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    })
+    
+    const data = {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      images
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(Yup.object().shape({
+        path: Yup.string().required()
+      }))
+    })
+
+    await schema.validate(data, {
+      abortEarly: false
+    })
+
+    const orphanage = orphanageRepository.create(data)
+  
+    await orphanageRepository.save(orphanage)
+  
+    return res.status(201).json(orphanage);
+  }
+}
---
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Marker, Popup } from 'react-leaflet';
+import L from 'leaflet'
+import { Link } from "react-router-dom";
+import { FiArrowRight, FiPlus } from "react-icons/fi";
+
+import mapMarkerImg from "../../assets/images/map-marker.svg";
+import Map from '../../components/Map';
+
+import './styles.css';
+
+const happyMapIcon = L.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+})
+
+export default function OrphanagesMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita </p>
+        </header>
+
+        <footer>
+          <strong>Ceará</strong>
+          <span>Mulungu</span>
+        </footer>
+      </aside>
+
+      <Map>
+        <Marker icon={happyMapIcon} position={[-4.3032032,-38.9981043]}>
+          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+            Lar das meninas
+            <Link to={`/orphanages/1`}>
+              <FiArrowRight size={20} color="#fff" />
+            </Link>
+          </Popup>
+        </Marker>
+      </Map>
+
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
+}
